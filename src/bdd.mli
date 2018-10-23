ae3c3e09634c07268875a849a7cfc50432a803a2
Steffen Smolka <smolka@cs.cornell.edu>
2018-10-22 23:52:46 -0400

    tweaks

--- src/bdd.mli
+++ src/bdd.mli
@@ -13,15 +13,23 @@ val equal : t -> t -> bool
 type manager
 val manager : unit -> manager
 
+
 (** {2 Constructors} *)
+
 val ctrue : t
 val cfalse : t
 
+
 (** {2 Boolean operations} *)
 
-val conj : manager -> t -> t -> t (** boolean conjunction (logical and) *)
-val disj : manager -> t -> t -> t (** boolean disjunction (logical or) *)
-val neg : manager -> t -> t       (** boolean negation (logical not) *)
+(** boolean conjunction (logical and) *)
+val conj : manager -> t -> t -> t
+
+(** boolean disjunction (logical or) *)
+val disj : manager -> t -> t -> t
+
+(** boolean negation (logical not) *)
+val neg : manager -> t -> t
 
 (** [ite mgr i u v] behaves like [u] when variable [i] is true, and like [v]
     otherwise.*)
@@ -30,6 +38,7 @@ val ite : manager -> int -> t -> t -> t
 (** BDDs form a boolean algebra. *)
 module Make () : Boolean.Algebra
   with type t = t
+
 
 (** {2 Semantics} *)
 
