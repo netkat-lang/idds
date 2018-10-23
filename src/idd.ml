f961d28dd18180afc9ab8ea689b2eaed1b64c4ee
Steffen Smolka <smolka@cs.cornell.edu>
2018-10-23 19:19:36 -0400

    smart Idd constructor

--- src/idd.ml
+++ src/idd.ml
@@ -27,9 +27,30 @@ let manager () : manager = {
   neg_cache = Hashtbl.create (module Int);
 }
 
-(* let branch mgr var hi lo =
-  if Dd.equal hi lo then
-    hi
-  else
-    Dd.branch mgr var hi lo *)
 
+let branch mgr var hi lo =
+  let i = Dd.(var.idx) in
+  match i % 2 with
+  | 0 -> (* var is input variable *)
+    if equal hi lo then hi else
+    let i' = i + 1 in
+    let hi = match hi with
+      | Branch { hi; lo=False; var={idx}; _ } when idx = i' -> hi
+      | _ -> hi
+    in
+    let lo = match lo with
+      | Branch { hi=False; lo; var={idx}; _ } when idx = i' -> lo
+      | _ -> lo
+    in
+    begin match hi, lo with
+    | Branch { hi=False; lo=l; var={idx}; _ }, _ when idx = i' && equal lo l -> lo
+    | _, Branch { hi=h; lo=False; var={idx}; _ } when idx = i' && equal hi h -> hi
+    | _ -> if equal hi lo then hi else Dd.branch mgr var hi lo
+    end
+  | 1 -> (* var is output variable *)
+    begin match hi, lo with
+    | False, False -> hi
+    | _ -> Dd.branch mgr var hi lo
+    end
+  | _ ->
+    assert false
