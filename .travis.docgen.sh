7579ae2009098bf7ba8c6011edd2d1c961d39e51
Steffen Smolka <smolka@cs.cornell.edu>
2019-08-09 23:38:27 +0200

    travis docgen: remove old docs

--- .travis.docgen.sh
+++ .travis.docgen.sh
@@ -23,6 +23,7 @@ rm -rf $DOCDIR
 git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} $DOCDIR 2>&1 | sed -e "s/$GH_TOKEN/!REDACTED!/g"
 git -C $DOCDIR checkout gh-pages || git -C $DOCDIR checkout --orphan gh-pages
 
+git rm -r '*'
 DOCFILES=$(echo $DOCSRC/*)
 cp -R $DOCFILES $DOCDIR
 
